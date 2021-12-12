baca9c8750fcababb5c303deedf17746b1c6bfd8
user.email <Kevin.danielm06@gmail.com>
2021-12-12 21:04:54 +0100

    Debug assembly dans web.config

--- GestionnaireVideothequeApp/Properties/AssemblyInfo.cs
+++ GestionnaireVideothequeApp/Properties/AssemblyInfo.cs
@@ -2,10 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Les informations générales relatives à un assembly dépendent de 
-// l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-[assembly: AssemblyTitle("GestionnaireVideothequeApp")]
+
+    // Les informations générales relatives à un assembly dépendent de 
+    // l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+    // associées à un assembly.
+    [assembly: AssemblyTitle("GestionnaireVideothequeApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
